d0d7bff79d0cd2e2d6010c7f1a068bc8f299487c
Kazamos <axel.maupome@aol.com>
2025-11-05 11:14:39 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import './style.css'
 
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
 
 // const app = createApp(App);
